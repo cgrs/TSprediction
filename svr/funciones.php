16932d04c1ec24b577831be682b2038da2e723a6
aaronpr95 <aaronp.morales@gmail.com>
2017-12-12 19:09:45 +0100

    ahora se recogen los datos (targets y samples) de un fichero json

--- svr/funciones.php
+++ svr/funciones.php
@@ -26,12 +26,37 @@ function print_targets($targets) {
 
 // funcion para grabar un array json en un fichero
 // para enviarle el formato de archivo a jose ignacio
-function grabar_array($array, $nom_file) {
+// el parametro log si esta activo nos muestra la confirmacion de que se ha grabado correctamente	
+function grabar_array($array, $nom_file, $log=true) {
 	$handle = fopen($nom_file, "w+");
 
 	$mi_json = json_encode($array);
+	$fwrite = fwrite($handle, $mi_json); // se graba el json en el fichero
 
 	fclose($handle); // se cierra el fichero
+
+	// se comprueba que se ha grabado correctamente
+	if ($log) {
+		if ($fwrite != false) {
+			echo "Se han escrito ".$fwrite." bytes.<br>";
+		} else {
+			echo "ERROR, no se ha grabado correctamente <br>";
+		}
+	}
+
+	return $fwrite;
+}
+
+// funcion para leer los datos del fichero json
+function get_datos ($nom_file="datos.json") {
+	$handle = fopen($nom_file, "r");
+
+	$datos_json = fread($handle, filesize($nom_file));
+	$datos = json_decode($datos_json, true);
+
+	fclose($handle); // se cierra el fichero
+
+	return $datos;
 }
 
 ?>
--- svr/pruebas.php
+++ svr/pruebas.php
@@ -6,8 +6,14 @@ require_once 'vendor/autoload.php';
 use Phpml\Regression\SVR;
 use Phpml\SupportVectorMachine\Kernel;
 
-$samples = [[60], [61], [62], [63], [65]]; // muestras
-$targets = [3.1, 3.6, 3.8, 4, 4.1];		   // objetivos
+// echo "OPCIONES<br>";
+// echo "<hr>";
+
+// $samples = [[60], [61], [62], [63], [65]]; // muestras
+// $targets = [3.1, 3.6, 3.8, 4, 4.1];		   // objetivos
+$datos = get_datos();
+$samples = $datos['samples'];
+$targets = $datos['targets'];
 $predict = 66;	// elemento a predecir
 
 $regression = new SVR(Kernel::LINEAR);
@@ -24,7 +30,12 @@ echo "Elemento a predecir: ".$predict."<br>";
 echo "Resultado: ".$result;
 echo "<hr>";
 
-echo json_encode($targets);
-echo json_encode($samples);
+$mis_datos = ['samples' => $samples, 'targets' => $targets];
+// echo json_encode($targets);
+// echo json_encode($samples);
+// echo json_encode($datos);
+
+// Se graban los datos en un fichero .json
+//grabar_array($mis_datos, "datos.json");
 
 ?>
