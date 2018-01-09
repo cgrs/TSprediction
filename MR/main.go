3dfe46b703edda65fa77bb927e8f4c5fb7b4fd38
pablogarpas <pablogp696@gmail.com>
2018-01-09 16:43:19 +0100

    Usando directory relativo para los datos y cambio en el formato de la fecha

--- MR/main.go
+++ MR/main.go
@@ -8,16 +8,17 @@ import (
 	"github.com/sajari/regression"
 	"strings"
 	"strconv"
+	"math"
 )
 
 func main() {
 	reg := new(regression.Regression)
-	reg.SetObserved("Murders per annum per 1,000,000 inhabitants")
-	reg.SetVar(0, "Día")
-	reg.SetVar(1, "Mes")
-	reg.SetVar(2, "Año")
+	reg.SetObserved("Voltaje consumido diario")
+	reg.SetVar(0, "fecha")
 	
-	file, err := os.Open("data.txt")
+	var fecha float64
+	
+	file, err := os.Open("../data/data.csv")
 	if err!=nil{
 		log.Fatal(err)
 	}
@@ -50,59 +51,70 @@ func main() {
 			//reg.AddCross(PowCross(0, 2))
 		}
 		
-		array := strings.Split(time,"/")
+		array := strings.Split(time,"/")		
 		
-		d,err:= strconv.ParseFloat(array[0],64)
+		d:= array[0]
+		m:= array[1]	
+		a:= array[2]
+		
+		varia:= d + m + a
+		
+		fecha,err=strconv.ParseFloat(varia,64)
 		if err!=nil{
-			fmt.Println("Error en d")
-			log.Fatal(err)
-		}
-		m,err:= strconv.ParseFloat(array[1],64)
-		if err!=nil{
-			fmt.Println("Error en m")
-			log.Fatal(err)
-		}
-		a,err:= strconv.ParseFloat(array[2],64)
-		if err!=nil{
-			fmt.Println("Error en a")
+			fmt.Println("Error en la conversión")
 			log.Fatal(err)
 		}
 			
 		reg.Train(
-			regression.DataPoint(voltaje, []float64{d,m,a}),
+			regression.DataPoint(voltaje, []float64{fecha}),
 		)
 
 		//fmt.Println("Fecha",time,"Voltaje:",value)
 	}
-	fmt.Println("Todos los datos han sido procesados entrenando a la máquina:")
+	fmt.Println("Datos procesados")
+	
 	reg.Run()
 	
-	prediction1, err := reg.Predict([]float64{22, 6, 2007})
+	prediction1, err := reg.Predict([]float64{12122009})
 	if err!=nil{
 		log.Fatal(err)
 	}
 	
-	fmt.Println(prediction1)
+	fmt.Println(math.Abs(prediction1))
 	
-		prediction2, err := reg.Predict([]float64{3, 1, 1999})
+		prediction2, err := reg.Predict([]float64{12122012})
 	if err!=nil{
 		log.Fatal(err)
 	}
 	
-	fmt.Println(prediction2)
+	fmt.Println(math.Abs(prediction2))
 	
-		prediction3, err := reg.Predict([]float64{1, 7, 2012})
+		prediction3, err := reg.Predict([]float64{112014})
 	if err!=nil{
 		log.Fatal(err)
 	}
 	
-	fmt.Println(prediction3)
+	fmt.Println(math.Abs(prediction3))
 	
-		prediction4, err := reg.Predict([]float64{22, 11, 2017})
+		prediction4, err := reg.Predict([]float64{12132015})
 	if err!=nil{
 		log.Fatal(err)
 	}
 	
-	fmt.Println(prediction4)
+	fmt.Println(math.Abs(prediction4))
+	
+	prediction5, err := reg.Predict([]float64{672017})
+	if err!=nil{
+		log.Fatal(err)
+	}
+	
+	fmt.Println(math.Abs(prediction5))
+	
+	prediction6, err := reg.Predict([]float64{442019})
+	if err!=nil{
+		log.Fatal(err)
+	}
+	
+	fmt.Println(math.Abs(prediction6))
 	
 }
